543711239a388b51c5dbe1825e60ce2105f2efc3
Saurabh Kumar Verma <svworld01@gmail.com>
2020-08-13 12:13:05 +0530

    Update FindTheDuplicateNumber.java

--- day1/FindTheDuplicateNumber.java
+++ day1/FindTheDuplicateNumber.java
@@ -1,6 +1,51 @@
-package day1;
-import java.util.Arrays;
-class Solution5 {
+/*
+Auther - Saurabh Verma
+Problem link - https://leetcode.com/problems/find-the-duplicate-number/
+
+there are three methods that I have found
+-------------------------------------------------------------------------
+
+#Method1- 
+O(n) Time Complexity
+using A[x] = -A[x] to check duplicate
+*/
+class Solution {
+    public int findDuplicate(int[] nums) {
+         for(int n:nums){
+            int index = Math.abs(n) - 1;
+           if(nums[index]<0){
+               return Math.abs(n);
+           }
+            nums[index]*=-1;
+        }
+         return 0;
+    }
+}
+/*
+#Method 2   
+O(n) time complexity
+using BitSet in Java
+*/
+class Solution {
+   public int findDuplicate(int[] nums) {
+        BitSet bs = new BitSet(nums.length);
+        for (int num : nums) {
+            if (bs.get(num)) {
+                return num;
+            } else {
+                bs.set(num, true);
+            }
+        }
+        
+        return -1;
+    }
+}
+/*
+#Method 3   
+O(nlogn) time complexity
+using BinarySearch method
+*/
+class Solution {
     public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         int result = nums[0];
