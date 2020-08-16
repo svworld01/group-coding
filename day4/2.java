49719a430960e9a67559fd7ba1030646f5642fd5
Saurabh Verma <svworld01@gmail.com>
2020-08-16 15:56:04 +0530

    Update 2.java

--- day4/2.java
+++ day4/2.java
@@ -41,3 +41,26 @@ class Solution {
         return -(lb +1);
     }
 }
+/*
+Method 2 in O(n+m)
+Logic - Kumar Shanu
+*/
+class Solution {
+    public int countNegatives(int[][] grid) {
+       
+        int m = grid.length;
+        int n = grid[0].length;
+        int i = m-1;
+        int j = 0;
+        int count = 0;
+        while( i>= 0  && j<n){
+            if( grid[i][j] < 0 ){
+                count += n - j;
+                i--;
+            }else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
