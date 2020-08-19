c7a183bdeabb62e91c6a882e0d96be5e28e61d14
akashcoderr <42908574+akashcoderr@users.noreply.github.com>
2020-08-19 01:10:56 -0700

    Update LinkedLIstCycle.java

--- day7/LinkedLIstCycle.java
+++ day7/LinkedLIstCycle.java
@@ -18,13 +18,18 @@ public class Solution {
         
         ListNode p1=head;
         ListNode p2=p1.next;
+        
+        //checking list is exist or not
         if(head==null){
             return false;
         }
+     //find a cycle in list
         while(p1!=p2){
+         //checking last node pointer
             if(p2==null||p2.next==null){
                 return false;
             }
+         //if thus is not last point to the next both pointer p1 and p2
             p1=p1.next;
             p2=p2.next.next;
         }
