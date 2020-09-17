4809876c8e2f69ffc6bb88006562cb5086fd16ec
Saurabh Verma <svworld01@gmail.com>
2020-09-17 22:03:29 +0530

    Create Solution2.java

---
+++ day17/Solution2.java
@@ -0,0 +1,17 @@
+/*
+Solution By - Saurabh Verma
+Problem link - https://leetcode.com/problems/contains-duplicate-ii/
+*/
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
