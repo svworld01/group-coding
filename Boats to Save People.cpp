fd4cab3fddcb93d6e960036867b7927238f57ce6
vivek0455 <69623792+vivek0455@users.noreply.github.com>
2020-09-17 13:36:40 +0530

    Create Boats to Save People.cpp
    Solution

---
+++ Boats to Save People.cpp
@@ -0,0 +1,32 @@
+ /*
+ code by : Vivek pandey
+ 
+ problem link : https://leetcode.com/problems/boats-to-save-people/
+ 
+ */
+ 
+ solution
+ 
+ 
+ class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int n = people.size()-1;
+        //people.push_back(99999999);
+        int i=0;
+        
+        int ant=0;
+        while(i<=n){
+            if(people[i] + people[n]<=limit){
+                i++;
+               n--;
+            }
+            else{
+                n--;
+            }
+            ant++;
+        }
+        return ant;
+    }
+};
